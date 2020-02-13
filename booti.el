c5fb485b77a0620a2916e739ba7d3ab7f763b14c
Alex Afshar <contact@alexafshar.com>
2020-02-12 22:15:50 -0500

    Stubbed out some framework functions. Added macro.
    Created out temporary functions for rails and spring-boot. Added a macro
    to generate scaffold function if it is a known function.

--- booti.el
+++ booti.el
@@ -14,7 +14,9 @@
 
 (defun scaffold-rails (dir name)
   "Create the scaffolding for Rails project in DIR with NAME."
-  (message "Running rails new in %s for %s." dir name))
+  (if  (executable-find "rails")
+      (shell-command "echo \"hello\"")
+      (message "rails command not found.")))
 
 (defun scaffold-spring-boot (dir name)
   "Create the scaffolding for Spring Boot project in DIR with NAME."
@@ -26,18 +28,25 @@ This will take in the FRAMEWORK name as a string,
 downcase the string and return as a symbol."
   (intern (downcase framework)))
 
+(defmacro generate-scaffold-function (name)
+  "Takes NAME for framework or language. If exists, will return its corresponding scaffold function."
+  `(intern (concat "scaffold-" (symbol-name ,name))))
+
 (defun booti (framework dir proj)
   "This is the 'top-level' function.
 It will be used to start the bootstrap process.
 It takes in FRAMEWORK, DIR, and PROJ in order to create the project scaffold."
+  ;; Get the framework name, directory, and project name.
   (interactive "sWhat framework would you like to Bootstrap? \nDIn which directory? \nsWhat will the project be called? ")
+  ;; Validate the framework name.
   (setq framework (validate-framework framework))
-  (progn
-    (message "The framework you entered was: %s" (symbol-name framework))
-    (sit-for 2)
-    (message "The directory you choose was: %s" (concat dir proj))
-    (sit-for 2)
-    (message "The project name was: %s" proj)))
+  ;; Check if the framework is known.
+  (let ((fn (cdr (assoc framework *framework-scaffold-commands*))))
+    (if fn
+        (funcall (generate-scaffold-function framework) dir proj)
+      (message "Framework %s not found." framework)))
+  ;; if known, call its scaffold function.
+  )
 
 
 (provide 'booti)
